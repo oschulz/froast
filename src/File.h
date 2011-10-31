4cdea58eed1f93c5a203efdc70da9c564ed443d8
Tobias Köttig <tobias.koettig@tu-dortmund.de>
2011-10-31 15:38:41 +0100

    Documentation of File.h

--- src/File.h
+++ src/File.h
@@ -25,30 +25,48 @@
 
 namespace froast {
 
-
+///	@brief	Class to manage file names
+	
 class File {
 	std::string m_path;
 
 public:
+	///	@brief	Returns path of the file.
 	std::string path() { return m_path; }
+	///	@brief	Returns path of the file.
 	std::string toString() { return m_path; }
+	///	@brief	Returns basename of the file. 
+	/**	Preceding paths will be stripped.*/
 	std::string basename() const;
+	///	@brief	Returns directory part of the filename
 	std::string dirname() const;
+	///	@brief	Returns file name stripped from the file extension. 
+	/**	Returns full file name if no extension was recognised. */
 	std::string label() const;
+	///	@brief	Returns file extension
 	std::string extension() const;
 	
+	///	@brief	Returns File initialised with the directory name of the current file
 	File dir() const { return File(dirname()); }
+	///	@brief	Returns File initialised with the basename of the current file
 	File base() const { return File(basename()); }
 	
+	///	@brief	Concatenate a File with another one
+	/**	File("/root/data") / File("filename.root") -> /root/data/filename.root */
 	File operator/(const std::string &part) const;
+	///	@brief	Combine two Files
 	File operator+(const std::string &part) const;
+	///	@brief	Insert tag between label and extension of File
 	File operator%(const std::string &tag) const;
-	
+	///	@brief	Cast to std::string
 	operator std::string() const { return m_path; }
 	
+	///	@brief	Returns current directory
 	static File currentDir();
+	///	@brief	Returns the users home directory
 	static File homeDir();
 
+	///	@brief	Constructor
 	File(const std::string &filePath) : m_path(filePath) {}
 	virtual ~File() {}
 };
